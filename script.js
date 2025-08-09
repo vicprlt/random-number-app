4cd3865a15dec2dc8cda8c99918e5bea00d39df1
Victorine Periolat <victorine.periolat@hotmail.fr>
2025-08-09 15:43:11 +0200

    update script.js to include min max

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 
 
 const randomNumberEl= document.querySelector("#random-number");
+const resultEl = document.querySelector("#result");
+const min = document.querySelector("#min");
+const max = document.querySelector("#max");
 
 randomNumberEl.addEventListener("click", () => {
-    document.querySelector("#result").textContent = Math.floor(
-        getRandomNumber(1,100)
+    resultEl.textContent = Math.floor(
+        getRandomNumber(Number(min.value),Number(max.value) + 1)
     );
 });
 
