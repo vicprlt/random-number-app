4547059c358af2ee8d46c7a45227044441b89b84
Victorine Periolat <victorine.periolat@hotmail.fr>
2025-08-09 14:46:53 +0200

    add function

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ console.log("Hey");
 const randomNumberEl= document.querySelector("#random-number");
 
 randomNumberEl.addEventListener("click", () => {
-    document.querySelector("#result").textContent = Math.random();
+    document.querySelector("#result").textContent = Math.floor(
+        getRandomNumber(1,100)
+    );
 });
+
+function getRandomNumber(min, max){
+    return Math.random() * (max - min) + min;
+}
